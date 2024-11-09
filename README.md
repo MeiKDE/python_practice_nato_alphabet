7d9770c4832dc375eb6399264dfb4a102c411acc
Mei Zhang <rubymxzhang@gmail.com>
2024-11-09 23:06:48 +0800

    inital commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# NATO Phonetic Alphabet Project
+
+This Python project demonstrates how to use the `pandas` library for handling dictionaries and data frames, and how to create a phonetic alphabet dictionary using a CSV file. The project includes examples of iterating through dictionaries and data frames, as well as using dictionary comprehension to convert data into a specific format.
+
+## Project Structure
+
+- **student_dict**: A dictionary containing students' names and their scores.
+- **student_data_frame**: A data frame created from `student_dict` to demonstrate row-wise iteration.
+- **phonetic_dict**: A dictionary created from a CSV file of the NATO phonetic alphabet, mapping letters to their corresponding code words.
---
+++ main.py
@@ -0,0 +1,51 @@
+import pandas
+
+student_dict = {"student": ["Angela", "James", "Lily"], "score": [56, 76, 98]}
+
+# Looping through dictionaries:
+for key, value in student_dict.items():
+    # Access key and value
+    print(key)
+    print(value)
+    pass
+
+student_data_frame = pandas.DataFrame(student_dict)
+
+# Loop through rows of a data frame
+for index, row in student_data_frame.iterrows():
+    # Access index and row
+    # Access row.student or row.score
+    print(row.student)
+    print(row.score)
+    pass
+
+# Loop through rows of a data frame
+for index, row in student_data_frame.iterrows():
+    if row.student == "Angela":
+        print(f"test row.score: {row.score}")
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+data = pandas.read_csv(
+    "/Users/mei/projects/python_practice_nato_alphabet/nato_phonetic_alphabet.csv"
+)
+print(data)
+print(data.to_dict())
+
+
+# use dictionary comprehension to get it to be the format we want
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+word = input("Enter a word?").upper()
+
+output_list = [phonetic_dict[letter] for letter in word]
+
+print(output_list)
